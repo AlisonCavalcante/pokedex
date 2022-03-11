abbe8505fef3e03b1bd8bd7cf287f5a714c0baab
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-03-11 11:06:09 -0300

    add método get genérico

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  urlBase: string = "https://pokeapi.co/api/v2/pokemon";
+
+  constructor(private http: HttpClient) { }
+
+  getPokemons(){
+    return this.http.get(`${this.urlBase}?limit=10`);
+  }
+
 }
