286e562b681ed3d00089392274065d92a501bc27
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-03-29 20:30:26 -0300

    add url mega evoluções

--- src/app/utils/constantes.ts
+++ src/app/utils/constantes.ts
@@ -2,5 +2,6 @@ export class Constantes{
 
   public static URLBASE = "https://pokeapi.co/api/v2/pokemon/";
   public static URLEVOLUCOES = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  public static MEGAEVOLUCOES = " https://pokeapi.co/api/v2/pokemon-species/";
 
 }
